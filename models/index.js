124f6f9de50e8efa0d262b04744a379d4e5be855
zynbahmed <201901241@student.polytechnic.bh>
2024-03-12 11:35:48 +0300

    fixes

--- models/index.js
+++ models/index.js
@@ -3,9 +3,9 @@ const coursesScheme = require("./Course")
 const studentsSchema = require("./Student")
 const gradesSchema = require("./Grade")
 
-const Course = mongoose.model("ThemePark", coursesScheme)
-const Student = mongoose.model("WaterPark", studentsSchema)
-const Grade = mongoose.model("WaterPark", gradesSchema)
+const Course = mongoose.model("Course", coursesScheme)
+const Student = mongoose.model("Student", studentsSchema)
+const Grade = mongoose.model("Grade", gradesSchema)
 
 module.exports = {
   Course,
