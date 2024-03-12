d88c825b5441cb4989b48813a3b3a51c3c9e1d30
zynbahmed <201901241@student.polytechnic.bh>
2024-03-12 11:05:39 +0300

    set up

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+
+const CoursesRouter = require('./routes/coursesRouter')
+const StudentskRouter = require('./routes/studentsRouter')
+
+const PORT = process.env.PORT || 3001
+
+const db = require('./db')
+
+const app = express()
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/courses', CoursesRouter)
+app.use('/students', StudentskRouter)
+
+app.use('/', (req, res) => {
+  res.send(`Connected!`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Running Express server on Port ${PORT} . . .`)
+})
